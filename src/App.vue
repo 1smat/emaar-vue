4440cfee55353bc4cddcacb5e9e2c038099c342d
Ismat Asatov <ismatasatov@Air-Ismat.home>
2023-04-06 22:37:21 +0500

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+
+import Container from './components/01-Container.vue'
+import Header from './components/02-Header.vue'
+
+</script>
+
+<template>
+  <Container>
+    <h1>child item</h1>
+    <Header />
+
+  </Container>
+</template>
+
+
---
+++ src/components/02-Header.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+
+defineProps<{ msg?: string }>()
+
+
+</script>
+
+<template>
+    <header class="header">
+        <h1>header h1</h1>
+    </header>
+</template>
+
+<style scoped lang="scss">
+.header {
+    h1 {
+        color: red;
+    }
+}
+</style>
